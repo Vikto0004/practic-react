f4dcd52cf7e84797fecf4b487cc2b124cb743473
Dmytro <57775385+YevichDmytro@users.noreply.github.com>
2024-06-29 13:05:36 +0300

    finish task-2

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,11 +1,17 @@
 import { Form, TodoList } from 'components';
 import { useState } from 'react';
 import { nanoid } from 'nanoid';
+import { EditForm } from '../components/EditForm/EditForm';
+import { Notify } from 'notiflix';
 
 export const Todos = () => {
   const [todos, setTodos] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [currentTodo, setCurrentTodo] = useState({});
 
   const addTodo = value => {
+    if (findTodo(value)) return;
+
     const todo = {
       id: nanoid(),
       text: value,
@@ -18,11 +24,56 @@ export const Todos = () => {
     setTodos(todos.filter(todo => todo.id !== id));
   };
 
-  console.log(todos);
+  const handleEditTodo = todo => {
+    setIsEditing(!isEditing);
+    setCurrentTodo({ ...todo });
+  };
+
+  const handleCancelUpd = () => {
+    setIsEditing(!isEditing);
+    setCurrentTodo({});
+  };
+
+  const handleUpdateTodo = value => {
+    if (findTodo(value)) return;
+    setTodos(
+      todos.map(todo => {
+        return todo.id === currentTodo.id
+          ? { ...currentTodo, text: value }
+          : todo;
+      })
+    );
+    handleCancelUpd();
+  };
+
+  const findTodo = text => {
+    const isTodoExist = todos.find(
+      todo => todo.text.toLowerCase() === text.toLowerCase()
+    );
+
+    if (isTodoExist) {
+      Notify.info(`todo ${text} is already created`);
+    }
+
+    return isTodoExist;
+  };
+
   return (
     <>
-      <Form onSubmit={addTodo} />
-      <TodoList data={todos} onDeleteTodo={onDeleteTodo} />
+      {isEditing ? (
+        <EditForm
+          defaultValue={currentTodo.text}
+          updateTodo={handleUpdateTodo}
+          cancelUpdate={handleCancelUpd}
+        />
+      ) : (
+        <Form onSubmit={addTodo} />
+      )}
+      <TodoList
+        data={todos}
+        onDeleteTodo={onDeleteTodo}
+        onEditTodo={handleEditTodo}
+      />
     </>
   );
 };
