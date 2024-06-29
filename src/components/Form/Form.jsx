79863c8e1ec6f24ff994d0eff578a6a0a8e46bb2
Vikto0004 <vikto0004@gmail.com>
2024-06-29 10:07:36 +0200

    Update

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,30 @@
-// import { FiSearch } from 'react-icons/fi';
+import { FiSearch } from 'react-icons/fi';
+import style from './Form.module.css';
+import { useState } from 'react';
 
-export const Form = () => {
-  return <h2>Form</h2>;
+export const Form = ({ onSubmit }) => {
+  const [word, setWord] = useState('');
+  const handleSubmit = e => {
+    e.preventDefault();
+    onSubmit(word);
+    setWord('');
+  };
+
+  return (
+    <form className={style.form} onSubmit={handleSubmit}>
+      <button className={style.button} type="submit">
+        <FiSearch size="16px" />
+      </button>
+
+      <input
+        className={style.input}
+        placeholder="What do you want to write?"
+        name="search"
+        value={word}
+        required
+        autoFocus
+        onChange={e => setWord(e.target.value)}
+      />
+    </form>
+  );
 };
--- src/tabs/Photos.jsx
+++ src/tabs/Photos.jsx
@@ -1,10 +1,28 @@
-// import { getPhotos } from 'apiService/photos';
-import { Text } from 'components';
+import { getPhotos } from 'apiService/photos';
+import { Form } from 'components';
+import { useEffect, useState } from 'react';
 
 export const Photos = () => {
+  const [query, setQuery] = useState('');
+  const [pictures, setPictures] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    if (!query) return;
+    getPhotos(query, page).then(({ photos, total_results }) => {
+      setPictures(photos);
+      setTotal(total_results);
+    });
+  }, [query, page]);
+
+  const onSubmit = data => {
+    setQuery(data);
+  };
+
   return (
     <>
-      <Text textAlign="center">Let`s begin search 🔎</Text>
+      <Form onSubmit={onSubmit} />
     </>
   );
 };
